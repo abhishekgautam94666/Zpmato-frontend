8bd2eee68b4acaadd901681d0d3e4d00fd96e678
Abhishek <abhishekgautam94666@gmail.com>
2024-08-04 16:41:54 +0530

    protected route

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import React, { Suspense } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Home from "./pages/Home.jsx";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Navbar from "./Components/Navbar.jsx";
 import Success from "./pages/Success.jsx";
 import Cancel from "./pages/Cancel.jsx";
-// import SingIn from "./pages/SingIn.jsx";
-// import Login from "./pages/Login.jsx";
-// import Profile from "./pages/Profile.jsx";
-// import MyResto from "./pages/MyResto.jsx";
-// import AddResto from "./pages/AddResto.jsx";
-// import RestoAddMenu from "./pages/RestoAddMenu.jsx";
-// import OrderOnline from "./pages/OrderOnline.jsx";
-// import MenuRes from "./pages/MenuRes.jsx";
+import { useSelector } from "react-redux";
 
 const Home = React.lazy(() => import("./pages/Home.jsx"));
 const SingIn = React.lazy(() => import("./pages/SingIn.jsx"));
@@ -24,6 +21,9 @@ const OrderOnline = React.lazy(() => import("./pages/OrderOnline.jsx"));
 const MenuRes = React.lazy(() => import("./pages/MenuRes.jsx"));
 
 function App() {
+  const { currentUser } = useSelector((state) => state.user);
+
+  //const navigate = useNavigate();
   return (
     <Router>
       <Suspense fallback={<div>Loading...</div>}>
@@ -31,7 +31,10 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/signIn" element={<SingIn />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route
+            path="/profile"
+            element={currentUser ? <Profile /> : <Navigate to={"/login"} />}
+          />
           <Route path="/addResto" element={<MyResto />} />
           <Route path="/addResto/restoAdd" element={<AddResto />} />
           <Route path="/profile/:restoId" element={<RestoAddMenu />} />
@@ -40,8 +43,8 @@ function App() {
             path="/restaurant/order-online/:name/:_id"
             element={<MenuRes />}
           />
-        <Route path="/success" element={<Success />} />
-        <Route path="/cancel" element={<Cancel />} />
+          <Route path="/success" element={<Success />} />
+          <Route path="/cancel" element={<Cancel />} />
         </Routes>
       </Suspense>
       <Navbar />
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Header1 from "../Components/Header1";
-import { signInSuccess } from "../redux/user/userSlice.js";
+import { currUser, signInSuccess } from "../redux/user/userSlice.js";
 import { useSelector, useDispatch } from "react-redux";
 import toast from "react-hot-toast";
 import MyResato from "../Components/MyResato.jsx";
@@ -76,6 +76,7 @@ const Profile = () => {
     const data = await res.json();
     if (data.success == true) {
       dispatch(signInSuccess(null));
+      dispatch(currUser());
       toast.success(data.message);
       navigate("/login");
     } else {
